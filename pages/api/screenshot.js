d0b5f477e592a246862749fbd9dc159025f008b7
Stefan Pejcic <6408320+stefanpejcic@users.noreply.github.com>
2025-08-28 13:29:03 +0200

    Update screenshot.js

--- pages/api/screenshot.js
+++ pages/api/screenshot.js
@@ -1,27 +1,23 @@
-import puppeteer from 'puppeteer-core';
-import chromium from 'chrome-aws-lambda';
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 
 export default async function handler(req, res) {
   const { url } = req.query;
-
-  if (!url) {
-    return res.status(400).json({ error: 'URL is required' });
-  }
+  if (!url) return res.status(400).json({ error: "URL is required" });
 
   try {
     const browser = await puppeteer.launch({
       args: chromium.args,
-      executablePath: await chromium.executablePath,
+      executablePath: await chromium.executablePath(),
       headless: chromium.headless,
     });
 
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'networkidle0' });
+    await page.goto(url, { waitUntil: "networkidle0" });
     const screenshot = await page.screenshot();
-
     await browser.close();
 
-    res.setHeader('Content-Type', 'image/png');
+    res.setHeader("Content-Type", "image/png");
     res.status(200).send(screenshot);
   } catch (error) {
     res.status(500).json({ error: error.message });
